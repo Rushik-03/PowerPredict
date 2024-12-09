1fa4551f0e175b858dfb7ff49a2dd2ba3d0201c8
Rushik-03 <bodasairushik@gmail.com>
2024-12-09 07:57:08 +0530

    Done

--- FrontEnd/src/Navbar.js
+++ FrontEnd/src/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => (
       <nav className="navbar navbar-expand-sm navbar-dark fixed-top" style={{backgroundColor: 'rgb(22, 40, 54)'}}>
           <div className="container">
             <Link className="navbar-brand" to="/">
-              <p></p><p className="text-info"><img src="plant.png" alt="..." height="32"/>PowerPredict</p>
+              <p></p><p className="text-info">PowerPredict</p>
             </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
@@ -16,7 +16,7 @@ const Navbar = () => (
                   <Link className="nav-link" aria-current="page" to="/" id="navv">Home</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/yield" id="navv">Predict Yield</Link>
+                  <Link className="nav-link" to="/yield" id="navv">Predict consumption</Link>
                 </li>
                 {/* <li className="nav-item">
                   <Link className="nav-link" to="/data" id="navv">View Data</Link>
